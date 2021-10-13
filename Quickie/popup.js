28df17f733c5335c6846917ae9c19e52151283df
Absolute3 <60956557+Absolute3@users.noreply.github.com>
2021-10-13 19:12:13 +0530

    Add files via upload

---
+++ Quickie/popup.js
@@ -0,0 +1,11 @@
+document.getElementById('gmail').addEventListener('click', () => {
+  window.open('https://gmail.com')
+})
+
+document.getElementById('youtube').addEventListener('click', () => {
+  window.open('https://www.youtube.com/channel/UCiGITf39Kp_vzDbqcqE2MuQ')
+})
+
+document.getElementById('twitch').addEventListener('click', () => {
+  window.open('https://twitch.tv/absolute3_tv')
+})
